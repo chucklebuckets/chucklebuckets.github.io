1f304fc4f8624e9f542becf520c7e2c304fd9386
Charlie Miller <75151185+chucklebuckets@users.noreply.github.com>
2026-06-15 21:11:50 -0400

    whims workably done

--- js/whims.js
+++ js/whims.js
@@ -29,3 +29,38 @@ for (const entry of entries) {
         cycleInterval = null;
     });
 }
+
+const imgPlaceholder = document.querySelector(".img-placeholder")
+const imgDisplay = document.querySelector(".img-display");
+const imgContent = document.querySelector(".content");
+const imgBorder = document.querySelector(".img-border")
+const imgThoughtBubble = document.querySelector(".img-thought-bubble")
+const imgWords = document.querySelectorAll(".img-word")
+
+const borderImgs = {
+    square: "/assets/whims/1x1_border.gif",
+    wide: "/assets/whims/4x3_border.gif",
+    tall: "/assets/whims/4x5_border.gif"
+};
+
+const borderThoughtBubbles = {
+    square: "/assets/whims/thought_bubble_red.gif",
+    wide: "/assets/whims/thought_bubble_blue.gif",
+    tall: "/assets/whims/thought_bubble_green.gif"
+}
+
+for (const word of imgWords) {
+    word.addEventListener("mouseenter", () => {
+        const imgPath = word.dataset.img;
+        const imgShape = word.dataset.shape;
+        
+        imgPlaceholder.style.display = "none";
+
+        imgDisplay.classList.remove("square", "wide", "tall");
+        imgDisplay.classList.add("show", imgShape);
+
+        imgThoughtBubble.src = borderThoughtBubbles[imgShape];
+        imgContent.src = imgPath;
+        imgBorder.src = borderImgs[imgShape];
+    });
+}
